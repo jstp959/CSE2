4187809fe4443ee412d3ecda310048114038fc5f
jstp959 <jes418@lehigh.edu>
2015-01-24 20:27:34 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+///////////////////////////////////
+//John St Pierre
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      java WelcomeClas.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println(" ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-J--E--S--4--1--8->");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("I really enjoy doing impressions. Elmo and JayZ are my favorites.");
+    
+    //test/testtest
+    }
+    
+}
